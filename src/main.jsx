d8ea6d8884ead68c6910dab9df766e1603f3f33c
THEVENARD Julien <thevenard.julien@gmail.com>
2024-03-08 21:15:54 +0100

    Test BrowserRouter with basename for githubpages

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router.jsx";
 import { DarkModeProvider } from "./context/DarkModeContext.jsx";
 import "./main.scss";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <DarkModeProvider>
-    <RouterProvider router={router} />
-  </DarkModeProvider>
+  <BrowserRouter basename="/OC_JT_P12_MyPortfolioV2">
+    <DarkModeProvider>
+      <RouterProvider router={router} />
+    </DarkModeProvider>
+  </BrowserRouter>
 );
